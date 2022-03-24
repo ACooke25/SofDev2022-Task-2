3f8f10e0b3a5dd4a392bc3c5e671521799bd6715
ACooke25 <102114309+ACooke25@users.noreply.github.com>
2022-03-24 12:19:01 +1100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SAC_1_Task_2_2nd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.dgvSales = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSales)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Location = new System.Drawing.Point(12, 70);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(108, 33);
+            this.btnChooseFile.TabIndex = 0;
+            this.btnChooseFile.Text = "Choose File";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // dgvSales
+            // 
+            this.dgvSales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSales.Location = new System.Drawing.Point(12, 156);
+            this.dgvSales.Name = "dgvSales";
+            this.dgvSales.RowHeadersWidth = 51;
+            this.dgvSales.RowTemplate.Height = 24;
+            this.dgvSales.Size = new System.Drawing.Size(854, 231);
+            this.dgvSales.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(698, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculate Total Profit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvSales);
+            this.Controls.Add(this.btnChooseFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSales)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.DataGridView dgvSales;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SAC_1_Task_2_2nd
+{
+    public partial class Form1 : Form
+    {
+        float total = 0f;
+        public Form1()
+        {
+            InitializeComponent();
+            dgvSales.ColumnCount = 7;
+            dgvSales.Columns[0].Name = "Textbook";
+            dgvSales.Columns[1].Name = "Subject";
+            dgvSales.Columns[2].Name = "Seller";
+            dgvSales.Columns[3].Name = "Purchaser";
+            dgvSales.Columns[4].Name = "Purchase Price";
+            dgvSales.Columns[5].Name = "Sale Price";
+            dgvSales.Columns[6].Name = "Profit";
+            
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                List<String> lines = new List<String>();
+                lines = File.ReadAllLines(filePath).ToList();
+                
+
+                foreach (String line in lines)
+                {
+                    List<string> items = line.Split(',').ToList();
+                    float profit;
+                    float purchasedPrice = float.Parse(items[4]);
+                    string salePrice = items[5];
+
+                    if (float.TryParse(salePrice, out float _salePrice))
+                    {
+
+                        
+                        profit = _salePrice - purchasedPrice;
+                    }
+                    else
+                    {
+
+                        profit = purchasedPrice * -1;
+                    }
+                    total += profit;
+
+                    items.Add(profit.ToString());
+                    dgvSales.Rows.Add(items.ToArray());
+                }
+
+               
+                
+               
+
+
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dgvSales.Rows.Add();
+            int rowCount = dgvSales.Rows.Count;
+            dgvSales.Rows[rowCount - 1].Cells[5].Value = ("Total Profit is: $").ToString();
+            dgvSales.Rows[rowCount - 1].Cells[6].Value = total.ToString();
+        }
+    }
+
+}
+
+
